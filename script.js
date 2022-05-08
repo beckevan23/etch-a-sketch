124062adb1694d954fa43a60c4490a0015dbe71b
nefarox <evan.beck23@gmail.com>
2022-05-07 21:15:35 -0500

    Add grid size-change function to button click

--- script.js
+++ script.js
@@ -11,35 +11,29 @@ function removeChildren() {
     };
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 sizeButton.addEventListener('click', function () {
     let value = +prompt('Enter a number < 100', 16);
-    if (value % 2 === 0 && value <= 100) {
+    let s = 100 / value;
+    let fr = '1fr ';
+    if (value <= 100) {
+        p = 1;
+        i = 1;
         removeChildren();
         function cloneSquare() {
-            for (i = 1; i <= value; ++i) {
+            for (i = 1; i <= value * value; ++i) {
                 let clone;
                 clone = square.cloneNode();
-                clone.classList.add(i);
+                clone.style.aspectRatio = '1/1';
+               /*  clone.style.width = `${s}%`;
+                clone.style.height = `${s}%`; */
+                clone.style.border = '.5px solid #000000';
                 grid.appendChild(clone);
             };
-        };
+            for (p = 1; p < value; p++)
+            fr += '1fr ';
+            grid.style.gridTemplateColumns = fr;
+            console.log(fr)
+        }; 
         cloneSquare();
     };
 });
-/* cloneSquare() CALL THIS FUNCTION IN A CLICK EVENT ON THE BUTTON USING PROMPT VALUE FOR LOOP*/
